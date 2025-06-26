c3e7a3cb9aa5fc06a4b5554d81dbde6bfb08f01c
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-26 15:45:31 +0700

    Menjalakan PostOrder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -121,3 +121,9 @@ class BinaryTree{
             }
             if(ptr == nullptr)
                 return;
+
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " "; //Parent
+        }
+};
